e1145e1dba8773879c05d8f07f2b638a6b772531
CanoCOL <canoloramiguel103@gmail.com>
2022-07-21 17:43:01 -0500

    se modifica el index y crea controles

---
+++ src/controllers/controlador.js
@@ -0,0 +1,11 @@
+//creando variables
+
+let nombreUsuario="Fabian Henao"
+const EDAD_USUARI=31
+let estaturaUsuario=1.73
+let colorUsuario=true
+
+console.log(nombreUsuario)
+ 
+EDAD_USUARIO=20
+console.log(EDAD_USUARIO)
