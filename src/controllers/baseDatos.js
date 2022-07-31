fa26bf2e400e32ae97903225dd334b08d8ecee25
fabian <fandresh62183@gmail.com>
2022-07-31 18:59:24 -0500

    se desarrolla proyecto

--- src/controllers/baseDatos.js
+++ src/controllers/baseDatos.js
@@ -1,74 +1,74 @@
 // QUEMAR (HARD CORE) LA BD
 let productos=[
     {
-        nombre:"CARBONERO BONSAI",
-        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/CARBONERO01.webp?alt=media&token=922e2d9f-7779-463d-abba-dab60b6b40d2","https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/CARBONEROFLORECIDO02.jpg?alt=media&token=36fc5b50-86b8-4269-b254-af90e23b7834"],
-        precio:0,
-        popularidad:0,
-        descripcion:""
+        nombre:"Bonsai de Junipero",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%231.jpg?alt=media&token=5e9c05fc-bdd6-4281-b42e-a0520a69eac0","https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%232.jpg?alt=media&token=3054d5a6-c63e-47d8-9fd9-c3c2c747284e"],
+        precio:35000000,
+        popularidad:"Bonsai de Junipero Chinensis o Enebro de 60 años de edad y con Altura de 145 cm",
+        descripcion:"Nativo del Himalaya, China, nordeste de Afganistán, este a oeste de Yunnan en el sudoeste de China, y con subpoblaciones aisladas en el norte al oeste de Gansu, y este de Fujian. Crece a altitudes de 1.600-4.900 msnm. El Juniperus es un género de más de treinta especies de coníferas arbustivas muy populares y adecuadas para ser utilizadas como Bonsai. La especie clásica es el ‘enebro chino’ (Juniperus chinensis), también populares son: Juniperus Squamata, Sargentii, Sabina, Rigida y Communis. La gama de colores de follaje oscila entre los verdes con tonos azul oscuro y los verdes claros. Estos árboles son muy adecuados para la creación de madera muerta (jin y shari)"
     },
     {
-        nombre:"CERINZO BONSAI",
-        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/CERINZO03.webp?alt=media&token=fef89bb6-6c2c-46a1-b031-90e366b0b517","https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/cerinzoflorecido04.jpg?alt=media&token=8620cf44-0932-4461-b6af-2b42693a8678"],
-        precio:78000,
-        popularidad:0,
-        descripcion:""
+        nombre:"bosai Haematoxylum campechianum",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%233.jpg?alt=media&token=defdfe32-3c68-416f-9506-0b916921424e","https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%234.jpg?alt=media&token=086b416f-14ac-4153-a0d7-5fe377a0b9dd"],
+        precio:15000000,
+        popularidad:"Bonsai de Brasil de 60 años y Altura 70 cm",
+        descripcion:"El palo de Campeche (Haematoxylum campechianum), o palo de tinte, es un árbol de la familia de las leguminosas. Es nativo de Mesoamérica, particularmente de la península de Yucatán y en especial del estado de Campeche, de Guatemala y Belice.El nombre de la especie quiere decir “madera que sangra” (haima en griego es sangre y xylon, madera). La madera se usa para extraer hematoxilin que es un colorante. También se usa para leña y para postes. Las hojas y ramas se usan como forraje. Las flores producen miel. Debido a sus flores atractivas se le usa como ornato alrededor de las casas.Es un árbol que llega a medir 6 metros de altura. Las hojas de 3-6 cm de largo son alternas, perennes, crasas y obtusas por su base. Las flores tienen cinco pétalos y corola blanca que se juntan en cimas sujetas por un pedúnculo. El fruto es una drupa de 1 cm de largo."
     },
     {
-        nombre:"GUAYACAN BONSAI",
-        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/GUAYACAN_05.webp?alt=media&token=257579e1-a0c9-4be5-b0b1-7b6321153d01","https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/GUAYACAN_06.webp?alt=media&token=bfb87c87-51da-4bd0-9f11-7a817b6ed9cb"],
-        precio:0,
-        popularidad:0,
-        descripcion:""
+        nombre:"Bonsai de Sombrero",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%235.jpg?alt=media&token=4870d0ad-54f4-42be-b95d-7f78e45c5730","https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%236.jpg?alt=media&token=9e67f9fc-0b58-4f2b-8f45-2c8927fa3584"],
+        precio:12000000,
+        popularidad:"Bonsai de Sombrero Chino de 50 años y altura de 70 cm",
+        descripcion:"La especie es originaria de Arunachal Pradesh, Assam, Bhutan, Darjeeling, Himachal  radesh, India, Jammu-Kashmir, Myanmar, Nepal, Sikkim y Uttaranchal, donde vive en las pendientes del Himalaya entre los 900 y 3000 m de altitud.El género está dedicado al botánico danés Johan Theodor Holmskiold (1731-1793); el  nombre de la especie es el adjetivo latino “sanguineus, a, um”= color sangre, con obvia referencia. Nombres comunes: chinaman’s hat, chinese hat plant, cup-and-saucer-plant, hat plant, mandarin’s hat, parasol flower (inglés); chapeau chinois (francés); cappello cinese(italiano); chapéu-chinês, chapéu-chinês-amarelo,chapéu-chinês-vernelho, chapéu-de-mandarim(portugués); bonete chino, paragüita chino, platillo, sombrilla china español).La Holmskioldia sanguinea Retz. (1791) es un arbusto siempre verde leñoso alto hasta cerca 5 m con largas. La Holmskioldia sanguinea es un arbusto tropical de 5 m con insólitas inflorescencias © Giuseppe Mazza ramas arqueadas, las jóvenes ligeramente pubescentes y en sección prácticamente cuadrangular. Las hojas son opuestas, ovadas o deltoides, con ápice largamente puntiagudo, ligeramente pubescentes, largas 5-12 cm y anchas 3-8 cm, con margen entero o ligeramente dentado, de color verde oscuro."
     },
     {
-        nombre:"",
-        fotos:["",""],
-        precio:0,
-        popularidad:0,
-        descripcion:""
+        nombre:"Bonsai de Ficus",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%237.jpg?alt=media&token=8dd0f584-5503-4894-8bc6-07c7011e5f68","https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%238.jpg?alt=media&token=ece5d340-df9e-4b72-a592-2e502fd9ed5b"],
+        precio:10000000,
+        popularidad:"Bonsai de Ficus Nerifolia de 30 años, Altura 70 cm",
+        descripcion:"Ficus neriifolia se llama ficus a un género compuesto por unas 900 especies vegetales, muchas de ellas arboles y arbustos de buen tamaño.Árbol de hoja perenne, bonito y muy apto para bonsái o mini-bonsái. Fácil de distinguir por la posesión de hojas estrechas y lanceoladas.Nombre latino: Ficus neriifolia Sinónimo: Ficus salicaria .Nombre común: Ficus nerifolia.Familia: Moraceae (Moráceas). Origen: Sudamérica. Luz: Buena iluminación pero evitando el sol directo, especialmente en verano.Si se cultivan en interior, se debe procurar que les dé la mayor cantidad posible de luz natural y artificial posible.Temperaturas: No resiste las heladas. Si se mantiene al aire libre, protegerla de temperaturas inferiores a 12ºC.Evitar los cambios bruscos de temperatura."
     },
     {
-        nombre:"",
-        fotos:[],
-        precio:0,
-        popularidad:0,
-        descripcion:""
+        nombre:"Bonsai de Mirto",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%238.jpg?alt=media&token=ece5d340-df9e-4b72-a592-2e502fd9ed5b","https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%2310.jpg?alt=media&token=58241d41-4ef1-45e5-868d-3552475c88ed"],
+        precio:6000000,
+        popularidad:"Bonsai de Mirto o Jazmín de naranja de 40 años y Altura de 90 cm",
+        descripcion:"Murraya paniculata es una planta tropical de hojas perennes pequeñas, flores blancas y perfumadas, que se cultiva como un árbol ornamental o de cobertura, originaria del Sureste de Asiadesde China a Japón y Malasia.La Murraya está estrechamente relacionada con los cítricos. Es un pequeño árbol tropical, de hoja perenne o arbusto que crece hasta 7 m de altura. La planta florece durante todo el año. Sus hojas son glabras y brillantes, produce hojas pinadas que curiosamente son elípticas a obovadas, cuneadas a rómbico. Las flores son terminales, corimbosas, pocas flores, densas y con perfume a jazmín. Los pétalos son 12-18 mm de largo, recurvadas y blancas (o de coloración crema). El fruto de Murraya paniculata es carnoso, oblongo ovoides, de color rojo a naranja, y crece hasta 2 a 3 cm de largo."
     },
     {
-        nombre:"",
-        fotos:[],
-        precio:0,
-        popularidad:0,
-        descripcion:""
+        nombre:"Bonsai de Sauce",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%2311.jpg?alt=media&token=d70a5c7b-bc40-4258-a442-d4a5c435e8a3","https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%2312.jpg?alt=media&token=feaad4bf-cb86-4ca2-b2ea-eb92233fc91c"],
+        precio:6000000,
+        popularidad:"Bonsai de Sauce de 30 años y Altura 65 cm",
+        descripcion:"Sauce     (Salix babylonica)  El tipo más conocido de sauce es el sauce llorón, con sus largas arrastran ramas que forman un dosel de paraguas perdido casi todo el camino hasta el suelo. Dentro del amplio conjunto de plantas leñosas de entre los que conocemos al sauce llorón, debemos decir que es uno de los más atractivos"
     },
     {
-        nombre:"",
-        fotos:[],
-        precio:0,
-        popularidad:0,
-        descripcion:""
+        nombre:"Bonsai de Kingsville",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%2313.jpg?alt=media&token=6715395b-7886-4dda-a478-e2c6e56182c7","https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%2314.jpg?alt=media&token=d7afd9ce-2bac-442a-94ed-e8f22fa80846"],
+        precio:5000000,
+        popularidad:"Bonsai de Kingsville Boxwood de 30 años y altura de 37 cm",
+        descripcion:"Originario de Japón, el Kingsville Boxwood es un árbol de crecimiento extremadamente lento. Tiene la hoja más pequeña de todos los bojes, razón por la cual es buscada para bonsai. El Kingsville Boxwood es un árbol de hoja perenne con hojas de color verde claro y tiene excelentes características de ramificación.Clima: Atlántico – Continental – Mediterráneo – Montañoso.Resistente a: Heladas Medias – Heladas Suaves – Suelo Alcalino.Tipo de suelo: pH Neutro – pH Alcalino – Suelo bien drenado – Suelo fértil – Textura arcillosa – Textura arenosa – Textura franca.Porte: Arbusto.Tipo: Hoja Perenne.Tipo de mantenimiento: Medio.Exposición: Semi-sol – Sombra luminosa – Sol.Necesidad de agua: Cantidad moderada de agua.Altura adulta: De 1-2m.Anchura adulta: 1-2m.Decoraciones de color: Amarillo y dorado – Verde claro – Verde Oscuro. decorativa por estación: Para Invierno – Para Otoño – Para Primavera – Para Verano.Floración: Primavera.Fructificación: Verano.Longevidad: De 30 – 100 años".
     },
     {
-        nombre:"",
-        fotos:[],
-        precio:0,
-        popularidad:0,
-        descripcion:""
+        nombre:"Bonsai de Junipero",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%2315.jpg?alt=media&token=c99df205-6486-4863-85e6-19d487316279","https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%2316.jpg?alt=media&token=b5ae5b84-2316-4ecd-afa2-d6ac91887be4"],
+        precio:4000000,
+        popularidad:"Bonsai de Junipero",
+        descripcion:"Juniperus squamata, es una especie arbórea de la familia de las coníferas (división Pinophyta).Nativo del Himalaya, China, nordeste de Afganistán, este a oeste de Yunnan en el sudoeste de China, y con subpoblaciones aisladas en el norte al oeste de Gansu, y este de Fujian. Crece a altitudes de 1.600-4.900 msnm. El Juniperus es un género de más de treinta especies de coníferas arbustivas muy populares y adecuadas para ser utilizadas como Bonsai. La especie clásica es el ‘enebro chino’ (Juniperus chinensis), también populares son: Juniperus Squamata, Sargentii, Sabina, Rigida y Communis. La gama de colores de follaje oscila entre los verdes con tonos azul oscuro y los verdes claros. Estos árboles son muy adecuados para la creación de madera muerta (jin y shari).Situación: coloque el árbol en el exterior, durante todo el año, en un lugar donde reciba una gran cantidad de luz solar. Los enebros no crecen bien en el interior. Durante el invierno debe protegerse el árbol cuando las temperaturas caen por debajo de -10 grados C."
     },
     {
-        nombre:"",
-        fotos:[],
-        precio:0,
-        popularidad:0,
-        descripcion:""
+        nombre:"onsai de Junipero",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%2317.jpg?alt=media&token=40056266-1e9d-43fd-9b38-905965f130de","https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%2318.jpg?alt=media&token=02a258a4-032d-4760-9a6b-627eb4846cb4"],
+        precio:3800000,
+        popularidad:"Bonsai de Junipero Itoigawa de 12 años de edad y con Altura de 24 cm",
+        descripcion:"Juniperus Chinensis variedad Itoigawa, es una especie arbórea de la familia de las coníferas (división Pinophyta). El junípero chinensis Itoigawa es uno de los árboles más valorados como bonsái debido a la densidad de su follaje, lo que lo hace especialmente indicado para crear diseños con volúmenes espectaculares.El Junípero es un género de alrededor de 50-70 especies dentro de la familia Ciprés.Son coníferas de hoja perenne en forma de árboles o arbustos, que son muy populares para los propósitos del Bonsái. Las especies más populares son el Junípero Chino (Juniperus Chinensis), Shimpaku Japonés (Juniperus Sargentii) y el Sonare (Juniperus procumbens). Estas especies presentan guías de cuidado similares.Los colores del follaje oscilan entre azules-verdes oscuros a verdes claros, y el follaje puede tener tanto forma de escamas, como de agujas. Los Juníperos de escama suelen tener un follaje en forma de aguja cuando son jóvenes (se le llama follaje juvenil), y el follaje típico en forma de escama aparece más tarde. Después de una poda o doblado fuertes, de un riego excesivo o de cualquier otro tipo de estrés, a menudo aparecerá de nuevo el follaje juvenil. Puede tardar unos pocos años hasta que el suficiente follaje en forma de escama haya crecido por completo y entonces se pueda eliminar todo el follaje en forma de aguja.Los conos en forma de baya son redondos u ovalados, dependiendo de la especie miden alrededor de 2cm (1 pulgada), o en ocasiones solo 3mm; y necesitan uno o dos años para madurar. Las semillas son redondas o con filos. Los conos a menudo se los comen las aves que esparcen a su vez las semillas germinables con sus excrementos.Los Juníperos son muy apropiados para crear madera muerta (Jin y Shari). Esto se debe a que las venas vivas debajo de una rama rota, o por cualquier otra razón moribunda, se secarán y morirán. Esto se traduce en una madera muerta natural que es pelada, pulida y blanqueada por las condiciones climáticas y que es muy duradera en el caso de los Juníperos. La tríada de follaje verde, corteza color rojo-marrón o amarillo-marrón y madera muerta de color blanco plateado es muy atrayente."
     },
     {
-        nombre:"",
-        fotos:[],
-        precio:0,
-        popularidad:0,
-        descripcion:""
+        nombre:"Bonsai de Premna",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%2319.jpg?alt=media&token=8aa38a55-361c-409d-8d3f-36d5787f0891","https://firebasestorage.googleapis.com/v0/b/tiendasurafaha.appspot.com/o/bonsai%2320.jpg?alt=media&token=3d58f241-a6b7-4b0c-bc98-4dca6363d4bd"],
+        precio:300000,
+        popularidad:"Bonsai de Premna de 7 años y Altura 14 cm",
+        descripcion:"Premna.Nombre científico.Premna Microphylla.Família Lamiáseas.Origen Planta subtropical proveniente de Indonesia, Malasia, Filipinas y Taiwan. Características Tronco de color crema claro. Por lo general retorcido. Hojas simples, alargadas, ligeramente ovaladas y de color verde intenso. Flores blancas, pequeñas y en racimo. Sus raíces son muy interesantes y fuertes, pudiéndose trabajar en estilo de “raíces expuestas”. Situación Deben situarse en el exterior, protegiéndolo del calor extremo del verano. Lo ideal es colocarlo en un lugar donde reciba el sol de la mañana y que esté protegido del calor del medio día. En el invierno debemos protegerlo ante las heladas."
     }
 
 ]
